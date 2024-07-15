2e455c4def277c1777aeb19c46cde10c5c6cfad3
Mandus Börjesson <borjessonmandus@gmail.com>
2024-07-15 22:19:34 +0200

    software: Counting up

--- software/src/main.rs
+++ software/src/main.rs
@@ -50,6 +50,135 @@ Gpio: OutputPin,
     }
 }
 
+//  * A *
+//  F   B
+//  * G *
+//  E   C
+//  * D *
+
+struct Digits<Gpio> {
+    a: Segment<Gpio>,
+    b: Segment<Gpio>,
+    c: Segment<Gpio>,
+    d: Segment<Gpio>,
+    e: Segment<Gpio>,
+    f: Segment<Gpio>,
+    g: Segment<Gpio>,
+}
+
+impl<
+Gpio: OutputPin,
+> Digits<Gpio> {
+    fn new(a: Segment<Gpio>, b: Segment<Gpio>, c: Segment<Gpio>, d: Segment<Gpio>, e: Segment<Gpio>, f: Segment<Gpio>, g: Segment<Gpio>) -> Self {
+        Self { a: a, b: b, c: c, d: d, e: e, f: f, g: g }
+    }
+
+    fn display(&mut self, digit: u16) {
+        match digit {
+            0 => {
+                self.a.set(true);
+                self.b.set(true);
+                self.c.set(true);
+                self.d.set(true);
+                self.e.set(true);
+                self.f.set(true);
+                self.g.set(false);
+            }
+            1 => {
+                self.a.set(false);
+                self.b.set(true);
+                self.c.set(true);
+                self.d.set(false);
+                self.e.set(false);
+                self.f.set(false);
+                self.g.set(false);
+            }
+            2 => {
+                self.a.set(true);
+                self.b.set(true);
+                self.c.set(false);
+                self.d.set(true);
+                self.e.set(true);
+                self.f.set(false);
+                self.g.set(true);
+            }
+            3 => {
+                self.a.set(true);
+                self.b.set(true);
+                self.c.set(true);
+                self.d.set(true);
+                self.e.set(false);
+                self.f.set(false);
+                self.g.set(true);
+            }
+            4 => {
+                self.a.set(false);
+                self.b.set(true);
+                self.c.set(true);
+                self.d.set(false);
+                self.e.set(false);
+                self.f.set(true);
+                self.g.set(true);
+            }
+            5 => {
+                self.a.set(true);
+                self.b.set(false);
+                self.c.set(true);
+                self.d.set(true);
+                self.e.set(false);
+                self.f.set(true);
+                self.g.set(true);
+            }
+            6 => {
+                self.a.set(true);
+                self.b.set(false);
+                self.c.set(true);
+                self.d.set(true);
+                self.e.set(true);
+                self.f.set(true);
+                self.g.set(true);
+            }
+            7 => {
+                self.a.set(true);
+                self.b.set(true);
+                self.c.set(true);
+                self.d.set(false);
+                self.e.set(false);
+                self.f.set(false);
+                self.g.set(false);
+            }
+            8 => {
+                self.a.set(true);
+                self.b.set(true);
+                self.c.set(true);
+                self.d.set(true);
+                self.e.set(true);
+                self.f.set(true);
+                self.g.set(true);
+            }
+            9 => {
+                self.a.set(true);
+                self.b.set(true);
+                self.c.set(true);
+                self.d.set(true);
+                self.e.set(false);
+                self.f.set(true);
+                self.g.set(true);
+            }
+            _ => {}
+        }
+    }
+
+    fn clear_pins(&mut self) {
+        self.a.clear_pins();
+        self.b.clear_pins();
+        self.c.clear_pins();
+        self.d.clear_pins();
+        self.e.clear_pins();
+        self.f.clear_pins();
+        self.g.clear_pins();
+    }
+}
 #[entry]
 fn main() -> ! {
     let mut p = pac::Peripherals::take().unwrap();
@@ -59,22 +188,54 @@ fn main() -> ! {
     let mut delay = Delay::new(cp.SYST, &rcc);
 
     let gpioa = p.GPIOA.split(&mut rcc);
+    let gpiob = p.GPIOB.split(&mut rcc);
 
-    // (Re-)configure PA1 as output
-    let mut segment = cortex_m::interrupt::free(|cs| {
-        Segment::new(gpioa.pa0.into_push_pull_output(cs).downgrade(),
-        gpioa.pa7.into_push_pull_output(cs).downgrade(),
+    let mut digits = cortex_m::interrupt::free(|cs| {
+        Digits::new(
+            // A
+            Segment::new(
+                gpioa.pa0.into_push_pull_output(cs).downgrade(),
+                gpioa.pa7.into_push_pull_output(cs).downgrade(),
+            ),
+            // B
+            Segment::new(
+                gpioa.pa1.into_push_pull_output(cs).downgrade(),
+                gpiob.pb0.into_push_pull_output(cs).downgrade(),
+            ),
+            // C
+            Segment::new(
+                gpioa.pa2.into_push_pull_output(cs).downgrade(),
+                gpiob.pb1.into_push_pull_output(cs).downgrade(),
+            ),
+            // D
+            Segment::new(
+                gpioa.pa3.into_push_pull_output(cs).downgrade(),
+                gpiob.pb2.into_push_pull_output(cs).downgrade(),
+            ),
+            // E
+            Segment::new(
+                gpioa.pa4.into_push_pull_output(cs).downgrade(),
+                gpioa.pa8.into_push_pull_output(cs).downgrade(),
+            ),
+            // F
+            Segment::new(
+                gpioa.pa5.into_push_pull_output(cs).downgrade(),
+                gpioa.pa11.into_push_pull_output(cs).downgrade(),
+            ),
+            // G
+            Segment::new(
+                gpioa.pa6.into_push_pull_output(cs).downgrade(),
+                gpioa.pa12.into_push_pull_output(cs).downgrade(),
+            )
         )
     });
 
-    let mut on = false;
-
     loop {
-        on = !on;
-        segment.set(on);
-        delay.delay_ms(100_u16);
-
-        segment.clear_pins();
-        delay.delay_ms(1000_u16);
+        for n in 0..10 {
+            digits.display(n);
+            delay.delay_ms(100_u16);
+            digits.clear_pins();
+            delay.delay_ms(1000_u16);
+        }
     }
 }
