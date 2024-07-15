1dd37c4346f11ac8f6ee1be4f370e35df3cd8b38
Mandus Börjesson <borjessonmandus@gmail.com>
2024-07-15 21:07:12 +0200

    software: doesnt work

--- software/src/main.rs
+++ software/src/main.rs
@@ -1,12 +1,15 @@
 #![no_main]
 #![no_std]
 
+extern crate alloc;
+use alloc::boxed::Box;
+
 use panic_halt as _;
 use stm32f0xx_hal as hal;
 use crate::hal::{delay::Delay, pac, prelude::*};
 use cortex_m;
 use cortex_m_rt::entry;
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::digital::{Error, OutputPin};
 
 #[derive(PartialEq)]
 enum SegmentState {
@@ -15,40 +18,47 @@ enum SegmentState {
     Unknown,
 }
 
-type SegmentPins<Set, Clear> = (Clear, Set);
+//  * A *
+//  F   B
+//  * G *
+//  E   C
+//  * D *
 
-struct Segment<SetPin: OutputPin, ClearPin: OutputPin> {
-    state: SegmentState,
-    pins: SegmentPins<SetPin, ClearPin>,
+trait DrivePins {
+    type T: OutputPin;
+    type U: OutputPin;
 }
 
-impl<
-SetPin: OutputPin,
-ClearPin: OutputPin,
-> Segment<SetPin, ClearPin> {
-    fn new(pins: SegmentPins<SetPin, ClearPin>) -> Self {
-        Self { state: SegmentState::Unknown, pins }
+struct Segment<Pins: DrivePins> {
+    state: SegmentState,
+    pin_set: Pins::U,
+    pin_clr: Pins::T,
+}
+
+impl <Pins: DrivePins> Segment<Pins> {
+    fn new(set: DrivePins::T, clear: DrivePins::U) -> Self {
+        Self { state: SegmentState::Unknown, pin_set: set, pin_clr: clear }
     }
 
     fn set(&mut self, on: bool) {
         let new_state = if on {SegmentState::On} else {SegmentState::Off};
 
-        // Energizing the coil at this point would only be a waste of energy, only actuate them
-        // when they are expected to change state.
+        // Energizing the coil at this point would be a waste of energy, only actuate them when
+        // they are expected to change state.
         if self.state == new_state {
             return;
         }
 
         match new_state {
-            SegmentState::On => { self.pins.1.set_high().ok(); }
-            SegmentState::Off => { self.pins.0.set_high().ok(); }
+            SegmentState::On => { (*self.pin_set).set_high().ok(); }
+            SegmentState::Off => { self.pin_clr.set_high().ok(); }
             _ => {}
         }
         self.state = new_state;
     }
     fn clear_pins(&mut self) {
-        self.pins.1.set_low().ok();
-        self.pins.0.set_low().ok();
+        self.pin_set.set_low().ok();
+        self.pin_clr.set_low().ok();
     }
 }
 
@@ -62,22 +72,19 @@ fn main() -> ! {
 
     let gpioa = p.GPIOA.split(&mut rcc);
 
-    // (Re-)configure PA1 as output
-    let pins = cortex_m::interrupt::free(|cs| {
-        (gpioa.pa0.into_push_pull_output(cs),
-        gpioa.pa7.into_push_pull_output(cs),
-        )
-    });
+    // let mut segment = cortex_m::interrupt::free(|cs| {
+    //     Segment::new(gpioa.pa0.into_push_pull_output(cs),
+    //         gpioa.pa7.into_push_pull_output(cs))
+    // });
 
-    let mut segment = Segment::new(pins);
     let mut on = false;
 
     loop {
         on = !on;
-        segment.set(on);
+        // segment.set(on);
         delay.delay_ms(100_u16);
 
-        segment.clear_pins();
+        // segment.clear_pins();
         delay.delay_ms(1000_u16);
     }
 }
