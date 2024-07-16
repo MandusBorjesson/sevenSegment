3db66a79df4fb6514e2665af1c9101be60da1a9e
Mandus Börjesson <borjessonmandus@gmail.com>
2024-07-16 15:13:31 +0200

    software: Move digit state cache to DigitController
    The application can handle any number of digits, and keeping that
    caching in the `Segment` struct makes little sense. Move if to the
    DigitController instead since it has a bit more context.

--- software/src/main.rs
+++ software/src/main.rs
@@ -9,16 +9,10 @@ use cortex_m_rt::entry;
 use embedded_hal::digital::v2::{OutputPin, PinState};
 
 const DIGIT_SELECTORS: usize = 3;
-
-#[derive(PartialEq)]
-enum SegmentState {
-    On,
-    Off,
-    Unknown,
-}
+const DIGITS: usize = 0x01 << DIGIT_SELECTORS;
+const SEGMENTS: usize = 7;
 
 struct Segment<Gpio> {
-    state: SegmentState,
     pin_set: Gpio,
     pin_clr: Gpio,
 }
@@ -27,158 +21,90 @@ impl<
 Gpio: OutputPin,
 > Segment<Gpio> {
     fn new(set: Gpio, clear: Gpio) -> Self {
-        Self { state: SegmentState::Unknown, pin_set: set, pin_clr: clear }
+        Self { pin_set: set, pin_clr: clear }
     }
 
     fn set(&mut self, on: bool) {
-        let new_state = if on {SegmentState::On} else {SegmentState::Off};
-
-        // Energizing the coil at this point would only be a waste of energy, only actuate them
-        // when they are expected to change state.
-        if self.state == new_state {
-            return;
+        match on {
+            true => {
+                self.pin_set.set_high().ok();
+                self.pin_clr.set_low().ok();
+            }
+            false => {
+                self.pin_set.set_low().ok();
+                self.pin_clr.set_high().ok();
+            }
         }
-
-        match new_state {
-            SegmentState::On => { self.pin_set.set_high().ok(); }
-            SegmentState::Off => { self.pin_clr.set_high().ok(); }
-            _ => {}
-        }
-        self.state = new_state;
     }
+
     fn clear_pins(&mut self) {
         self.pin_set.set_low().ok();
         self.pin_clr.set_low().ok();
     }
 }
 
-//  * A *
-//  F   B
-//  * G *
-//  E   C
-//  * D *
-
 struct DigitController<Gpio> {
-    a: Segment<Gpio>,
-    b: Segment<Gpio>,
-    c: Segment<Gpio>,
-    d: Segment<Gpio>,
-    e: Segment<Gpio>,
-    f: Segment<Gpio>,
-    g: Segment<Gpio>,
+    //   *  A/0  *
+    //  F/5     B/1
+    //   *  G/6  *
+    //  E/4     C/2
+    //   *  D/3  *
+    segments: [Segment<Gpio>; SEGMENTS],
+    state: [Option<[bool; SEGMENTS]>; DIGITS],
 }
 
 impl<
 Gpio: OutputPin,
 > DigitController<Gpio> {
     fn new(a: Segment<Gpio>, b: Segment<Gpio>, c: Segment<Gpio>, d: Segment<Gpio>, e: Segment<Gpio>, f: Segment<Gpio>, g: Segment<Gpio>) -> Self {
-        Self { a: a, b: b, c: c, d: d, e: e, f: f, g: g }
+        Self { segments: [a, b, c, d, e, f, g], state: [None; DIGITS] }
     }
 
-    fn display(&mut self, digit: u16) {
-        match digit {
-            0 => {
-                self.a.set(true);
-                self.b.set(true);
-                self.c.set(true);
-                self.d.set(true);
-                self.e.set(true);
-                self.f.set(true);
-                self.g.set(false);
+    fn update(&mut self, digit: usize, new_state: [bool; SEGMENTS]) {
+        if digit >= self.state.len() {
+            return;
+        }
+        match self.state[digit] {
+            None => {
+                for segment in 0..self.segments.len() {
+                    self.segments[segment].set(new_state[segment]);
+                }
             }
-            1 => {
-                self.a.set(false);
-                self.b.set(true);
-                self.c.set(true);
-                self.d.set(false);
-                self.e.set(false);
-                self.f.set(false);
-                self.g.set(false);
+            Some(old_state) => {
+                for segment in 0..self.segments.len() {
+                    // Energizing the coil if the state hasn't changed would be a waste of energy,
+                    // only actuate segments when they are expected to change state.
+                    if old_state[segment] != new_state[segment] {
+                        self.segments[segment].set(new_state[segment]);
+                    } else {
+                        self.segments[segment].clear_pins();
+                    }
+                }
             }
-            2 => {
-                self.a.set(true);
-                self.b.set(true);
-                self.c.set(false);
-                self.d.set(true);
-                self.e.set(true);
-                self.f.set(false);
-                self.g.set(true);
-            }
-            3 => {
-                self.a.set(true);
-                self.b.set(true);
-                self.c.set(true);
-                self.d.set(true);
-                self.e.set(false);
-                self.f.set(false);
-                self.g.set(true);
-            }
-            4 => {
-                self.a.set(false);
-                self.b.set(true);
-                self.c.set(true);
-                self.d.set(false);
-                self.e.set(false);
-                self.f.set(true);
-                self.g.set(true);
-            }
-            5 => {
-                self.a.set(true);
-                self.b.set(false);
-                self.c.set(true);
-                self.d.set(true);
-                self.e.set(false);
-                self.f.set(true);
-                self.g.set(true);
-            }
-            6 => {
-                self.a.set(true);
-                self.b.set(false);
-                self.c.set(true);
-                self.d.set(true);
-                self.e.set(true);
-                self.f.set(true);
-                self.g.set(true);
-            }
-            7 => {
-                self.a.set(true);
-                self.b.set(true);
-                self.c.set(true);
-                self.d.set(false);
-                self.e.set(false);
-                self.f.set(false);
-                self.g.set(false);
-            }
-            8 => {
-                self.a.set(true);
-                self.b.set(true);
-                self.c.set(true);
-                self.d.set(true);
-                self.e.set(true);
-                self.f.set(true);
-                self.g.set(true);
-            }
-            9 => {
-                self.a.set(true);
-                self.b.set(true);
-                self.c.set(true);
-                self.d.set(true);
-                self.e.set(false);
-                self.f.set(true);
-                self.g.set(true);
-            }
+        }
+        self.state[digit] = Some(new_state);
+    }
+
+    fn display_number(&mut self, digit: usize, number: usize) {
+        match number {
+            0 => { self.update(digit, [true, true, true, true, true, true, false]); }
+            1 => { self.update(digit, [false, true, true, false, false, false, false]); }
+            2 => { self.update(digit, [true, true, false, true, true, false, true]); }
+            3 => { self.update(digit, [true, true, true, true, false, false, true]); }
+            4 => { self.update(digit, [false, true, true, false, false, true, true]); }
+            5 => { self.update(digit, [true, false, true, true, false, true, true]); }
+            6 => { self.update(digit, [true, false, true, true, true, true, true]); }
+            7 => { self.update(digit, [true, true, true, false, false, false, false]); }
+            8 => { self.update(digit, [true, true, true, true, true, true, true]); }
+            9 => { self.update(digit, [true, true, true, true, false, true, true]); }
             _ => {}
         }
     }
 
     fn clear_pins(&mut self) {
-        self.a.clear_pins();
-        self.b.clear_pins();
-        self.c.clear_pins();
-        self.d.clear_pins();
-        self.e.clear_pins();
-        self.f.clear_pins();
-        self.g.clear_pins();
+        for segment in self.segments.iter_mut() {
+            segment.clear_pins();
+        }
     }
 }
 
@@ -194,13 +120,13 @@ Gpio: OutputPin,
         Self { pins_control: control, pin_enable: enable }
     }
 
-    fn strobe(&mut self, selected_digit: u32, delay: &mut Delay) {
+    fn strobe(&mut self, digit: usize, delay: &mut Delay) {
         // We can control a maximum of two to the power of "number of control pins" digits
-        if selected_digit >= 2_u32.pow(self.pins_control.len().try_into().unwrap()) {
+        if digit >= (0x01 << self.pins_control.len()) {
             return;
         }
         for n in 0..self.pins_control.len() {
-            let pin_state = if (selected_digit & 0x01 << n) > 0 {PinState::High} else {PinState::Low};
+            let pin_state = if (digit & 0x01 << n) > 0 {PinState::High} else {PinState::Low};
             self.pins_control[n].set_state(pin_state).ok();
         }
         self.pin_enable.set_high().ok();
@@ -261,7 +187,6 @@ fn main() -> ! {
                 )
             ),
             DigitSelector::new(
-                // S0
                 [
                     gpiob.pb5.into_push_pull_output(cs).downgrade(),
                     gpiob.pb4.into_push_pull_output(cs).downgrade(),
@@ -273,10 +198,10 @@ fn main() -> ! {
     });
 
     loop {
-        for n in 0..10 {
-            controller.display(n);
-            for i in 0..8 {
-                selector.strobe(i, &mut delay);
+        for number in 0..10 {
+            for digit in 0..8 {
+                controller.display_number(digit, (digit+number) % 10);
+                selector.strobe(digit, &mut delay);
             }
             controller.clear_pins();
             delay.delay_ms(1000_u16);
