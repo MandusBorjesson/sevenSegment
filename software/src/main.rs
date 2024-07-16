7f1f92683795c454c79f39d97f2decf325013d45
Mandus Börjesson <borjessonmandus@gmail.com>
2024-07-16 12:57:03 +0200

    software: Add digit selector
    Currently, this conflicts a bit with the caching in the `Segment` struct
    since it has no concept of a segment being present in multiple digits.

--- software/src/main.rs
+++ software/src/main.rs
@@ -6,7 +6,9 @@ use stm32f0xx_hal as hal;
 use crate::hal::{delay::Delay, pac, prelude::*};
 use cortex_m;
 use cortex_m_rt::entry;
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::digital::v2::{OutputPin, PinState};
+
+const DIGIT_SELECTORS: usize = 3;
 
 #[derive(PartialEq)]
 enum SegmentState {
@@ -56,7 +58,7 @@ Gpio: OutputPin,
 //  E   C
 //  * D *
 
-struct Digits<Gpio> {
+struct DigitController<Gpio> {
     a: Segment<Gpio>,
     b: Segment<Gpio>,
     c: Segment<Gpio>,
@@ -68,7 +70,7 @@ struct Digits<Gpio> {
 
 impl<
 Gpio: OutputPin,
-> Digits<Gpio> {
+> DigitController<Gpio> {
     fn new(a: Segment<Gpio>, b: Segment<Gpio>, c: Segment<Gpio>, d: Segment<Gpio>, e: Segment<Gpio>, f: Segment<Gpio>, g: Segment<Gpio>) -> Self {
         Self { a: a, b: b, c: c, d: d, e: e, f: f, g: g }
     }
@@ -179,6 +181,35 @@ Gpio: OutputPin,
         self.g.clear_pins();
     }
 }
+
+struct DigitSelector<Gpio> {
+    pins_control: [Gpio; DIGIT_SELECTORS],
+    pin_enable: Gpio,
+}
+
+impl<
+Gpio: OutputPin,
+> DigitSelector<Gpio> {
+    fn new(control: [Gpio; DIGIT_SELECTORS], enable: Gpio) -> Self {
+        Self { pins_control: control, pin_enable: enable }
+    }
+
+    fn strobe(&mut self, selected_digit: u32, delay: &mut Delay) {
+        // We can control a maximum of two to the power of "number of control pins" digits
+        if selected_digit >= 2_u32.pow(self.pins_control.len().try_into().unwrap()) {
+            return;
+        }
+        for n in 0..self.pins_control.len() {
+            let pin_state = if (selected_digit & 0x01 << n) > 0 {PinState::High} else {PinState::Low};
+            self.pins_control[n].set_state(pin_state).ok();
+        }
+        self.pin_enable.set_high().ok();
+        delay.delay_ms(100_u16);
+        self.pin_enable.set_low().ok();
+
+    }
+}
+
 #[entry]
 fn main() -> ! {
     let mut p = pac::Peripherals::take().unwrap();
@@ -190,51 +221,64 @@ fn main() -> ! {
     let gpioa = p.GPIOA.split(&mut rcc);
     let gpiob = p.GPIOB.split(&mut rcc);
 
-    let mut digits = cortex_m::interrupt::free(|cs| {
-        Digits::new(
-            // A
-            Segment::new(
-                gpioa.pa0.into_push_pull_output(cs).downgrade(),
-                gpioa.pa7.into_push_pull_output(cs).downgrade(),
+    let (mut controller, mut selector) = cortex_m::interrupt::free(|cs| {
+        (
+            DigitController::new(
+                // A
+                Segment::new(
+                    gpioa.pa0.into_push_pull_output(cs).downgrade(),
+                    gpioa.pa7.into_push_pull_output(cs).downgrade(),
+                ),
+                // B
+                Segment::new(
+                    gpioa.pa1.into_push_pull_output(cs).downgrade(),
+                    gpiob.pb0.into_push_pull_output(cs).downgrade(),
+                ),
+                // C
+                Segment::new(
+                    gpioa.pa2.into_push_pull_output(cs).downgrade(),
+                    gpiob.pb1.into_push_pull_output(cs).downgrade(),
+                ),
+                // D
+                Segment::new(
+                    gpioa.pa3.into_push_pull_output(cs).downgrade(),
+                    gpiob.pb2.into_push_pull_output(cs).downgrade(),
+                ),
+                // E
+                Segment::new(
+                    gpioa.pa4.into_push_pull_output(cs).downgrade(),
+                    gpioa.pa8.into_push_pull_output(cs).downgrade(),
+                ),
+                // F
+                Segment::new(
+                    gpioa.pa5.into_push_pull_output(cs).downgrade(),
+                    gpioa.pa11.into_push_pull_output(cs).downgrade(),
+                ),
+                // G
+                Segment::new(
+                    gpioa.pa6.into_push_pull_output(cs).downgrade(),
+                    gpioa.pa12.into_push_pull_output(cs).downgrade(),
+                )
             ),
-            // B
-            Segment::new(
-                gpioa.pa1.into_push_pull_output(cs).downgrade(),
-                gpiob.pb0.into_push_pull_output(cs).downgrade(),
-            ),
-            // C
-            Segment::new(
-                gpioa.pa2.into_push_pull_output(cs).downgrade(),
-                gpiob.pb1.into_push_pull_output(cs).downgrade(),
-            ),
-            // D
-            Segment::new(
-                gpioa.pa3.into_push_pull_output(cs).downgrade(),
-                gpiob.pb2.into_push_pull_output(cs).downgrade(),
-            ),
-            // E
-            Segment::new(
-                gpioa.pa4.into_push_pull_output(cs).downgrade(),
-                gpioa.pa8.into_push_pull_output(cs).downgrade(),
-            ),
-            // F
-            Segment::new(
-                gpioa.pa5.into_push_pull_output(cs).downgrade(),
-                gpioa.pa11.into_push_pull_output(cs).downgrade(),
-            ),
-            // G
-            Segment::new(
-                gpioa.pa6.into_push_pull_output(cs).downgrade(),
-                gpioa.pa12.into_push_pull_output(cs).downgrade(),
+            DigitSelector::new(
+                // S0
+                [
+                    gpiob.pb5.into_push_pull_output(cs).downgrade(),
+                    gpiob.pb4.into_push_pull_output(cs).downgrade(),
+                    gpiob.pb3.into_push_pull_output(cs).downgrade(),
+                ],
+                gpioa.pa15.into_push_pull_output(cs).downgrade(),
             )
         )
     });
 
     loop {
         for n in 0..10 {
-            digits.display(n);
-            delay.delay_ms(100_u16);
-            digits.clear_pins();
+            controller.display(n);
+            for i in 0..8 {
+                selector.strobe(i, &mut delay);
+            }
+            controller.clear_pins();
             delay.delay_ms(1000_u16);
         }
     }
